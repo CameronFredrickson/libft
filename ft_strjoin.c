b3df3dc1d8934112ca06e5c806af0b72eb9c732f
Cameron Fredrickson <cfredric@e1z2r2p8.42.us.org>
2016-11-20 11:10:04 -0800

    protected against NULL pointer argument

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -26,6 +26,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*cat;
 
+	if (!s1 || !s2)
+		return (NULL);
 	if (!(cat = ft_strnew(ft_strlen(s1) + ft_strlen(s2))))
 		return (NULL);
 	ft_strcpy(cat, s1);
